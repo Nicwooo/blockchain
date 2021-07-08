4a9682181a5fe8447b1f0551f9e6eb06b8f49c7f
Nicolas Sugranes <nicolas.sug@orange.fr>
2021-07-08 15:58:20 +0200

    Wrote all the needed methods structure

--- classes/Chain.py
+++ classes/Chain.py
@@ -4,3 +4,18 @@ class Chain:
 
     def __init__(self):
         pass
+
+    def generate_hash(self):
+        pass
+
+    def verify_hash(self):
+        pass
+
+    def add_block(self):
+        pass
+
+    def get_block(self):
+        pass
+
+    def add_transaction(self):
+        pass
