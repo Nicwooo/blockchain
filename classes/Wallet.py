4ce0f1cd811dbe9dde71cf919739ef6834929eb2
Nicolas Sugranes <nicolas.sug@orange.fr>
2021-07-08 11:11:36 +0200

    Added the basics attributes and methods for the wallet class

--- classes/Wallet.py
+++ classes/Wallet.py
@@ -3,7 +3,17 @@ import uuid
 
 class Wallet:
     unique_id = ''
+    balance = 100
+    history = []
 
-    @classmethod
-    def generate_unique_id(cls):
+    def generate_unique_id(self):
         self.unique_id = uuid.uuid1()
+
+    def add_balance(self, amount):
+        self.balance += amount
+
+    def sub_balance(self, amount):
+        self.balance -= amount
+
+    def send(self):
+        pass
