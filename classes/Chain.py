1ea5a0f133759312ba0bc2708a2d4793767ec6f0
Nicolas Sugranes <nicolas.sug@orange.fr>
2021-07-08 16:16:04 +0200

    Wrote the generate_hash, verify_hash and generate_random_string methods

--- classes/Chain.py
+++ classes/Chain.py
@@ -1,3 +1,12 @@
+import hashlib
+import os.path
+from random import randint
+
+
+def generate_random_string():
+    return randint(0, 1000)
+
+
 class Chain:
     blocks = []
     last_transaction_number = 0
@@ -6,10 +15,20 @@ class Chain:
         pass
 
     def generate_hash(self):
-        pass
+        string_to_test = generate_random_string()
+        hash = hashlib.sha256(string_to_test.encode()).hexdigest()
 
-    def verify_hash(self):
-        pass
+        if self.verify_hash(hash):
+            return hash
+        else:
+            self.generate_hash()
+
+    def verify_hash(self, hash_to_verify):
+        for i in self.blocks:
+            if self.blocks[i].hash == hash_to_verify:
+                return False
+
+        return hash_to_verify[:4] == '0000'
 
     def add_block(self):
         pass
